f5f57e4fa8b1114660d329a6fe19d128b5e7a4cb
Chanho Park <chanho61.park@samsung.com>
2014-07-18 18:06:33 +0900

    textdomain
    Signed-off-by: Chanho Park <chanho61.park@samsung.com>

--- libcpp/init.c
+++ libcpp/init.c
@@ -152,7 +152,7 @@ init_library (void)
       init_trigraph_map ();
 
 #ifdef ENABLE_NLS
-       (void) bindtextdomain (PACKAGE, LOCALEDIR);
+       (void) bindtextdomain (PACKAGE PACKAGE_SUFFIX, LOCALEDIR);
 #endif
     }
 }
