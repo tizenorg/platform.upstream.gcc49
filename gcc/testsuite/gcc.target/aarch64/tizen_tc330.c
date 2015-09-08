78dfb2025a36a5c8fbad989db8ebb65d0084016c
Maxim Ostapenko <m.ostapenko@partner.samsung.com>
2015-09-08 17:23:19 +0300

    Backport from Linaro GCC 4.9.2. @TC-330
    gcc/
    2015-01-11  Yvan Roux  <yvan.roux@linaro.org>
    
    	Backport from trunk r217362, r217546.
    	2014-11-14  Ramana Radhakrishnan  <ramana.radhakrishnan@arm.com>
    
    	PR target/63724
            * config/aarch64/aarch64.c (aarch64_expand_mov_immediate): Split out
            numerical immediate handling to...
            (aarch64_internal_mov_immediate): ...this. New.
            (aarch64_rtx_costs): Use aarch64_internal_mov_immediate.
            (aarch64_mov_operand_p): Relax predicate.
            * config/aarch64/aarch64.md (mov<mode>:GPI): Do not expand CONST_INTs.
            (*movsi_aarch64): Turn into define_insn_and_split and new alternative
            for 'n'.
            (*movdi_aarch64): Likewise.
    
    	2014-11-11  James Greenhalgh  <james.greenhalgh@arm.com>
    
    	* config/aarch64/aarch64-simd.md
    	(aarch64_simd_bsl<mode>_internal): Remove float cases, canonicalize.
    	(aarch64_simd_bsl<mode>): Add gen_lowpart expressions where we
    	are punning between float vectors and integer vectors.
    
    gcc/testsuite
    2015-01-11  Yvan Roux  <yvan.roux@linaro.org>
    
    	Backport from trunk r217362.
    	2014-11-11  James Greenhalgh  <james.greenhalgh@arm.com>
    
    	* gcc.target/aarch64/vbslq_f64_1.c: New.
    	* gcc.target/aarch64/vbslq_f64_2.c: Likewise.
    	* gcc.target/aarch64/vbslq_u64_1.c: Likewise.
    	* gcc.target/aarch64/vbslq_u64_2.c: Likewise.
    	* gcc.target/aarch64/tizen_tc330.c: Likewise.
    
    Change-Id: I8e093f22e01eb051783c17e7a401e8ff31c855c4
    git-svn-id: svn://gcc.gnu.org/svn/gcc/branches/linaro/gcc-4_9-branch@219433 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.target/aarch64/tizen_tc330.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-options "-save-temps -O2" } */
+
+#define VALUE 0xc521974f
+
+int foo (int *p, int *q)
+{
+  p[0] = VALUE;
+  q[0] = VALUE;
+}
+
+/* { dg-final { scan-assembler-times "movk\\tw2, 0xc521, lsl 16" 1 } } */
+/* { dg-final { cleanup-saved-temps } } */
